d6affd2e1663ba5308c337b64c9409161a4c5a04
blondie <mladenilic79@gmail.com>
2023-10-13 19:59:18 +0200

    postgres

--- cheat sheets/7 postgresql/postgress.sql
+++ cheat sheets/7 postgresql/postgress.sql
@@ -47,7 +47,7 @@ DROP DATABASE testingDatabase;
 -- create table
 CREATE TABLE car(
     car_id BIGSERIAL PRIMARY KEY,
-    make VARCHAR(100) NOT NULL,
+    make VARCHAR NOT NULL,
     model VARCHAR(100) NOT NULL,
     price NUMERIC(19,2) NOT NULL
 );
@@ -65,6 +65,8 @@ CREATE TABLE person(
     -- UNIQUE(price)
     car_id BIGINT REFERENCES car (car_id)
     -- UNIQUE(car_id) --  add unique if car can only be owned by one owner
+    -- car_id BIGINT REFERENCES car (car_id) ON DELETE CASCADE - add cascade or other options like this:
+    -- ON DELETE SET NULL, ON DELETE RESTRICT, ON DELETE NO ACTION, ON DELETE SET DEFAULT
 );
 
 -- delete table
@@ -77,17 +79,17 @@ ALTER TABLE person DROP CONSTRAINT person_gender_check;
 ALTER TABLE person DROP CONSTRAINT person_randomnumber_key;
 -- modiry constraints add
 ALTER TABLE person ADD PRIMARY KEY (id); -- add primary key - will work if there are no duplicates
-ALTER TABLE person ADD FOREIGN KEY (person_car_id_fkey) REFERENCES car (car_id);
--- modify constraints add (with setting custom name)
-ALTER TABLE person ADD CONSTRAINT unique_email UNIQUE(email); -- will work if there are no duplicates
-ALTER TABLE person ADD CONSTRAINT gender_check CHECK(gender = 'Female' OR gender = 'female' OR gender = 'Male' OR gender = 'male' OR gender = 'Agender' OR gender = 'Non-binary' OR gender = 'Genderqueer'); -- add checked constraint with choose name
+ALTER TABLE person ADD FOREIGN KEY (car_id) REFERENCES car (car_id);
 -- modify constraints add alternatively with letting database set the name for them
 ALTER TABLE person ADD UNIQUE(email); -- will work if there are no duplicates
-ALTER TABLE person ADD CHECK(gender = 'Female' OR gender = 'female' OR gender = 'Male' OR gender = 'male' OR gender = 'Agender' OR gender = 'Non-binary' OR gender = 'Genderqueer'); -- add checked constraint let db choose name for you
+ALTER TABLE person ADD CHECK(gender = 'Female' OR gender = 'female' OR gender = 'Male' OR gender = 'male' OR gender = 'Agender' OR gender = 'Non-binary' OR gender = 'Genderqueer' OR gender='Genderfluid'); -- add checked constraint let db choose name for you
+-- modify constraints add (with setting custom name)
+ALTER TABLE person ADD CONSTRAINT unique_email UNIQUE(email); -- will work if there are no duplicates
+ALTER TABLE person ADD CONSTRAINT gender_check CHECK(gender = 'Female' OR gender = 'female' OR gender = 'Male' OR gender = 'male' OR gender = 'Agender' OR gender = 'Non-binary' OR gender = 'Genderqueer' OR gender='Genderfluid'); -- add checked constraint with choose name
 
 -- alter table
-ALTER TABLE person ADD another_column TYPE VARCHAR(10);
-ALTER TABLE person ALTER COLUMN another_column SET NOT NULL;
+ALTER TABLE person ADD COLUMN another_column VARCHAR;
+ALTER TABLE person ALTER COLUMN another_column SET NOT NULL; -- this will work after populating the column
 ALTER TABLE person RENAME COLUMN another_column TO another_column_2;
 ALTER TABLE person DROP COLUMN another_column_2;
 ALTER TABLE person RENAME TO person2;
@@ -117,12 +119,20 @@ INSERT INTO person(
     'Marica', 'Smith', 'Male', 'jake@gmail.com', DATE '1993-02-03', 'London', 333
 ) ON CONFLICT (email) DO UPDATE SET email = EXCLUDED.email, first_name = EXCLUDED.first_name; -- this will work on unique column, and will prevent an error and update the field instead of doing nothing
 
+INSERT INTO person(first_name, last_name, gender, email, date_of_birth, country_of_birth, randomnumber)
+VALUES
+    ('Anne', 'Smith', 'Female', 'Kjake@gmail.com', DATE '1993-01-03', 'US', 444),
+    ('Bri', 'Smith', 'Female', 'Lbri@gmail.com', DATE '1989-01-03', NULL, 5.5),
+    ('Jack', 'Smith', 'Male', ';jake@gmail.com', DATE '1993-02-03', 'Paris', 888),
+    ('Marica', 'Smith', 'Male', 'JHjake@gmail.com', DATE '1993-02-03', 'London', 333);
+
 -- update whole table -- !!!!!!!!! this will update whole table don't do this !!!!!!!!!!
 UPDATE person SET email = 'omar@gmail.com'; -- !!!!!!!!! this will update whole table don't do this !!!!!!!!!!
 
 -- update records
 UPDATE person SET email = 'omar@gmail.com' WHERE id = 1;
 UPDATE person SET gender = 'Male', country_of_birth = 'Paris' WHERE id = 2;
+
 
 -- delete all inside table -- !!!!!!!!! this will empty table don't do this !!!!!!!!!!
 DELETE FROM person; -- !!!!!!!!!! this will empty table don't do this !!!!!!!!!!
@@ -131,6 +141,8 @@ TRUNCATE TABLE person; -- !!!!!!!!!! this will empty table don't do this !!!!!!!
 -- delete records
 DELETE FROM person WHERE id = 1;
 DELETE FROM person WHERE gender = 'Male' AND country_of_birth = 'Paris';
+
+-- input big files here
 
 -- select / from
 SELECT * FROM person;
@@ -147,16 +159,13 @@ SELECT country_of_birth AS country FROM person; -- as(alias) / coalesce(return d
 -- some functions
 SELECT first_name, coalesce(email, 'default value if value missing') FROM person; -- coalesce - default value
 -- handle null value - nullif returns 1st argument if both values are not equel, returns null if they are equal
-SELECT first_name, 10/NULLIF(1+1-2, 0) AS new_price FROM person; -- this will replace 0 with null to prevent division with zero
+SELECT first_name, 10/NULLIF(randomnumber, 0) AS new_price FROM person; -- this will replace 0 with null to prevent division with zero
 SELECT first_name, UPPER(first_name) AS upper_first_name, LOWER(first_name) AS lower_first_name FROM person;
 SELECT first_name, LENGTH(first_name) AS length_of_first_name FROM person;
 SELECT first_name, TRIM(first_name) AS trimmed_first_name FROM person;
-SELECT first_name || ' ' || last_name AS full_name FROM person;
 SELECT CONCAT(first_name, ' ', last_name) AS full_name, country_of_birth FROM person;
-SELECT first_name, last_name, date_of_birth, 
-    CASE
-        WHEN date_of_birth < DATE 1-1-1980 THEN 'old'
-        ELSE 'young';
+SELECT first_name || ' ' || last_name AS full_name FROM person; -- also concat
+SELECT first_name, GREATEST(randomnumber, 50), LEAST(randomnumber, 20) FROM person;
 
 -- +(add) / -(subtract) / *(multiply) / /(division) / ^(power) / %(moduo)
 SELECT randomnumber, randomnumber * 2 AS MULT, randomnumber / 2 AS DIV, randomnumber + randomnumber AS ADS, randomnumber - randomnumber AS SUB, randomnumber ^ 2 AS POWER, randomnumber % 3 AS MODUL FROM person;
@@ -174,33 +183,33 @@ SELECT EXTRACT(YEAR FROM NOW());
 SELECT EXTRACT(MONTH FROM NOW());
 SELECT EXTRACT(DAY FROM NOW());
 SELECT EXTRACT(DOW FROM NOW()); -- DOW - day of the week
-SELECT EXTRACT(MONTH FROM date_of_birth) AS month_of_birth FROM PERSON;
+SELECT first_name, EXTRACT(MONTH FROM date_of_birth) AS month_of_birth FROM PERSON;
 SELECT first_name, AGE(NOW(), date_of_birth) AS age FROM person; -- AGE returns time between two dates
 
 -- where / and / or / in / between / like / ilike / < > <= >= <> !=
-SELECT * FROM person WHERE date_of_birth >= DATE '1960-06-01';
-SELECT * FROM person WHERE date_of_birth > DATE '1960-06-01';
-SELECT * FROM person WHERE gender = 'Female' AND country_of_birth = 'Russia';
-SELECT * FROM person WHERE gender = 'Female' AND (country_of_birth != 'Russia' OR country_of_birth <> 'China');
-SELECT * FROM person WHERE country_of_birth IN ('Russia', 'China');
-SELECT * FROM person WHERE country_of_birth NOT IN ('Russia', 'China');
+SELECT * FROM person WHERE date_of_birth >= DATE '1990-06-01';
+SELECT * FROM person WHERE date_of_birth > DATE '1990-06-01';
+SELECT * FROM person WHERE gender = 'Non-binary' OR  country_of_birth = 'China';
+SELECT * FROM person WHERE gender != 'Female' AND gender <> 'Male';
+SELECT * FROM person WHERE country_of_birth IN ('Russia', 'France');
+SELECT * FROM person WHERE country_of_birth NOT IN ('Russia', 'China', 'Argentina', 'Indonesia', 'Brazil', 'Portugal', 'France');
 SELECT * FROM person WHERE date_of_birth BETWEEN DATE '2000-01-01' AND '2023-01-01';
 SELECT * FROM person WHERE email LIKE '%o.com'; -- % means any number of characters
 SELECT * FROM person WHERE email LIKE '%oo%';
 SELECT * FROM person WHERE email LIKE '_________@_________.com'; -- _ represents single character
 SELECT * FROM person WHERE country_of_birth ILIKE 'p%'; -- ILIKE same as LIKE just ignores case
-SELECT * FROM person WHERE email SIMILAR TO '%oo%'; -- SIMILAR TO same as like just interprets regex differently
-SELECT * FROM person WHERE email ~ '%oo%'; -- ~ same as like just interprets regex differently
+SELECT * FROM person WHERE email SIMILAR TO '%oo%'; -- SIMILAR TO - stronger then LIKE - interprets regex differently
+SELECT * FROM person WHERE email ~ '.+@ovh.net'; -- ~ more powerfull then SIMILAR TO - interprets regex differently
+SELECT * FROM person WHERE email = NULL;
 SELECT * FROM person WHERE email IS NULL;
 SELECT * FROM person WHERE email IS NOT NULL;
-SELECT * FROM person WHERE email = NULL;
 
 -- aggragate / group by / having (having is filtering of groups)
 
 -- count
 SELECT COUNT(*) FROM PERSON;
 SELECT country_of_birth, COUNT(*) FROM person GROUP BY country_of_birth;
-SELECT country_of_birth, COUNT(*) FROM person GROUP BY country_of_birth HAVING COUNT(*) > 5;
+SELECT country_of_birth, COUNT(*) FROM person GROUP BY country_of_birth HAVING COUNT(*) > 2;
 
 -- min / max / avg / sum / round
 SELECT MAX(randomnumber) FROM person;
@@ -208,8 +217,7 @@ SELECT MIN(randomnumber) FROM person;
 SELECT ROUND(AVG(randomnumber)) FROM person;
 SELECT SUM(randomnumber) FROM person;
 SELECT country_of_birth, MIN(randomnumber) FROM person GROUP BY country_of_birth;
-SELECT country_of_birth, MIN(randomnumber) FROM person GROUP BY country_of_birth HAVING MIN(randomnumber) >= 90;
-SELECT country_of_birth, SUM(randomnumber) FROM person GROUP BY country_of_birth HAVING SUM(randomnumber) <= 500;
+SELECT country_of_birth, SUM(randomnumber) FROM person GROUP BY country_of_birth HAVING SUM(randomnumber) <= 50;
 SELECT first_name, country_of_birth, MIN(randomnumber) FROM person GROUP BY first_name, country_of_birth; -- double grouping
 
 --order by / asc/desc
@@ -221,32 +229,26 @@ SELECT * FROM person ORDER BY first_name, last_name; -- combine order
 -- joins
 
 -- inner
-
 SELECT * 
 FROM person
     JOIN car ON person.car_id = car.car_id;
-    -- JOIN table_y ON table_x.id = table_y.id; -- add like this in case of joining multiple tables
+    -- JOIN table_y ON table_x.id = table_y.id; -- add like this in case of joining more tables
+    -- JOIN expression can have AND expression inside for more advanced joining
 
 -- inner join same as join
-SELECT * 
+SELECT *, person.first_name
 FROM person
     INNER JOIN car ON person.car_id = car.car_id;
-
--- specific columns
-SELECT person.first_name, car.make
-FROM person
-    JOIN car ON person.car_id = car.car_id;
 
 -- if both ids are the same keyword USING can be used
 SELECT *
 FROM person
     JOIN car USING(car_id);
 
--- natural join
+-- natural join, joining is done intuitively by db engine, discarding duplicate columns
 SELECT * 
 FROM person
-    NATURAL JOIN car ON person.car_id = car.car_id;
-
+    NATURAL JOIN car;
 
 -- left (take all from left)
 SELECT *
@@ -258,27 +260,34 @@ SELECT *
 FROM person
     RIGHT JOIN car ON person.car_id = car.car_id;
 
--- full outer join
+-- full outer join (is combination of left join & right join)
 SELECT *
 FROM person
-    FULL OUTER JOIN car ON person.car_id = car.car_id;
+    FULL JOIN car ON person.car_id = car.car_id;
 
 -- cross join (include each row from both tables)
 SELECT *
 FROM person
-    CROSS JOIN car ON person.car_id = car.car_id;
+    CROSS JOIN car;
 
--- unions for stacking data verticaly (default is to remove all duplicates)
+-- union for stacking data verticaly (data must be stackable for this to work)
+
+-- union (remove all duplicates)
 SELECT id, first_name, last_name, randomnumber FROM person
 UNION
 SELECT * FROM car
 UNION
 SELECT * FROM car;
 
--- unions for stacking data verticaly (keep all duplicates)
-SELECT id, first_name, last_name, randomnumber FROM person
+-- union all (keep all duplicates)
+SELECT id, first_name, last_name, randomnumber FROM person 
 UNION ALL
-SELECT id, first_name, last_name, randomnumber FROM person;
+SELECT id, first_name, last_name, randomnumber FROM person ORDER BY first_name;
+
+-- INTERSECT - find rows that are the same and remove duplicates
+-- INTERSECT ALL - find rows that are the same and keep the duplicates
+-- EXCEPT - find rows present in first query only and remove duplicates
+-- EXCEPT ALL - find rows present in first query only and keep the duplicates
 
 -- subqueries
 
@@ -288,7 +297,15 @@ SELECT a.first_name, a.last_name FROM (SELECT * FROM person) a; -- a refers to t
 -- subquery in where
 SELECT * FROM person WHERE country_of_birth IN (SELECT country_of_birth FROM person);
 
--- views (recalculations are not updated automagically so to update on call 
+-- all - true for all values returned by subquery
+SELECT * FROM person WHERE randomnumber > ALL (SELECT randomnumber FROM person WHERE country_of_birth = 'China');
+
+-- some / any - true for at least one element returned by subquery
+-- another subquery example here
+
+-- views
+
+-- recalculations are not updated automagically so to update on call 
 -- don't use aggregations, calculation functions, distinct, union, group by, having)
 CREATE VIEW custom_view AS
 SELECT * FROM person;
@@ -297,33 +314,47 @@ SELECT * FROM custom_view;
 -- delete view with
 DROP VIEW custom_view;
 
--- create function with
+-- functions - many ways to do this unfortunatelly :(
+
+
+
+
+
+
+
+
+
+
+
+-- body with ''
 CREATE OR REPLACE FUNCTION add_two_values(int, int) RETURNS int AS
 '
 SELECT $1 + $2;
 '
-LANGUAGE SQL
--- call function with
+LANGUAGE SQL;
 SELECT add_two_values(4,5);
 
--- create function with
+-- alternative body with $
 CREATE OR REPLACE FUNCTION add_two_values(int, int) RETURNS int AS
 $body$
     SELECT $1 + $2;
 $body$
-LANGUAGE SQL
--- call function with
+LANGUAGE SQL;
 SELECT add_two_values(4,5);
 
--- create function with (void means returns nothing)
+
+
+
+
+
+-- void means returns nothing
 CREATE OR REPLACE FUNCTION update_state() RETURNS void AS
 $body$
     UPDATE person
     SET first_name = "Cirilo"
-    WHERE country_of_birth = "China"
+    WHERE country_of_birth = "China";
 $body$
-LANGUAGE SQL
--- call function with
+LANGUAGE SQL;
 SELECT update_state();
 
 -- create function with (named parameters)
@@ -362,6 +393,16 @@ $body$
 LANGUAGE SQL
 -- call function with
 SELECT result_rows(); -- one field format
+
+
+
+
+
+
+
+
+
+
 
 -- PL/pgSQL
 CREATE OR REPLACE FUNCTION select_function(name_to_get VARCHAR) RETURNS VARCHAR AS
